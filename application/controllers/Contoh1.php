830689d757c8ff4de3e4f8a163e288120643f86a
farizrezas <muhamadfarizrezasaputra@hmail.com>
2021-09-29 21:51:06 +0700

    menambahkan application/,system/,user_guide/

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Muhamad Fariz Reza Saputra
+ Saya tingga di daerah Tangerang
+ olah raga yang saya sukai adalah
+ Futsal";
+ }
+}
