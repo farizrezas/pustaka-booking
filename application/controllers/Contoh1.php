54c66e033a6f4d2d72b6a7d7427754d20ee94a3e
dickisepto <dickisepto@gmail.com>
2021-10-26 17:25:07 +0700

    pembaruan 26-10-21

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,5 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
+        echo"Nama Dicki Septoari Husna
+        Saya tinggal di daerah Cikokol
+        kegiatan yang saya sukai adalah bermain game";
+       
     }
 }
